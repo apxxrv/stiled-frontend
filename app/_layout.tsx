d10919f9e4516916f5a0047f5ddcd5083b63ad9b
Apoorv Singh <108655991+apxxrv@users.noreply.github.com>
2025-06-18 02:12:06 -0700

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { Stack } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
-import '../global.css';
+import '../global.css'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,7 @@ const queryClient = new QueryClient({
 });
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <QueryClientProvider client={queryClient}>
       <StatusBar style="dark" />
